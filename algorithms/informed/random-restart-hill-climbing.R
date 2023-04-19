27ee8561ad9b8bba14e748237cf2e5fa07b5d38a
josugoar <jogoar@opendeusto.es>
2023-04-19 16:09:02 +0200

    Update rrhc

---
+++ algorithms/informed/random-restart-hill-climbing.R
@@ -0,0 +1,42 @@
+random.restart.hill.climbing = function(file,
+                                        restarts,
+                                        max_iterations = 50, 
+                                        count_print = 10, 
+                                        trace = FALSE) {
+  
+  name_method <- paste0("Random Restart Hill Climbing Search")
+  
+  # Get Start time
+  start_time  <- Sys.time()
+  
+  state_final <- NULL
+  
+  count <- 0
+  
+  while (count <= restarts) {
+    problem <- initialize.problem(file)
+    
+    result <- hill.climbing.search(problem,
+                                   max_iterations = max_iterations,
+                                   count_print = count_print,
+                                   trace = trace)
+    
+    if (is.null(state_final) || result$state_final$evaluation <= state_final$evaluation) {
+      state_final <- result$state_final
+      report      <- result$report
+    }
+    
+    count <- count + 1
+  }
+  
+  # Get runtime
+  end_time <- Sys.time()
+  
+  result <- list()
+  result$name        <- name_method
+  result$runtime     <- end_time - start_time
+  result$state_final <- state_final
+  result$report      <- report
+  
+  return(result)
+}
