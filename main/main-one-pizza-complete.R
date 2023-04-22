73be790fb5efc428ff3668e15c3815c0d9fa63dc
Josu <jogoar@opendeusto.es>
2023-04-22 15:11:53 +0200

    Update and rename main-p-hub.R to main-one-pizza-complete.R

---
+++ main/main-one-pizza-complete.R
@@ -0,0 +1,105 @@
+# Clear environment and console
+rm(list=ls())
+cat("\014")
+graphics.off()
+# Set working directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#Import the libraries needed to display the results
+library(kableExtra)
+library(magrittr)
+
+# Include algorithm functions
+source("../algorithms/blind/expand-node.R")
+source("../algorithms/informed/hill-climbing-search.R")
+source("../algorithms/informed/local-beam-search.R")
+source("../algorithms/informed/random-restart-hill-climbing.R")
+source("../algorithms/informed/stochastic-hill-climbing.R")
+source("../algorithms/informed/stochastic-random-restart.R")
+
+# Include functions for data analysis and result plot
+source("../algorithms/results-analysis/analyze-results.R")
+
+# Include the problem
+source("../problem/one-pizza-complete.R")
+
+# Executes hill climbing search and return the results
+execute.hill.climbing <- function(filename) {
+  # Initialize problem
+  problem <- initialize.problem(filename = filename)
+  # Execute hill climbing
+  return(hill.climbing.search(problem = problem))
+}
+
+# Executes hill climbing search and return the results
+execute.local.beam.search <- function(filename) {
+  # Initialize problem
+  problem <- initialize.problem(filename = filename)
+  # Execute local beam search
+  return(local.beam-search(problem = problem))
+}
+
+# Executes random restart hill climbing search and return the results
+execute.random.restart.hill.climbing <- function(filename) {
+  # Execute random restart hill climbing
+  return(random.restart.hill.climbing(file = filename))
+}
+
+# Executes stochastic hill climbing search and return the results
+execute.stochastic.hill.climbing <- function(filename) {
+  # Execute stochastic hill climbing
+  return(stochastic.hill.climbing.search(file = filename))
+}
+
+# Executes stochastic random restart search and return the results
+execute.stochastic.random.restart <- function(filename) {
+  # Execute stochastic random restart
+  return(stochastic.random.restart(file = filename))
+}
+
+# Execute several times and analyze results
+test <- function(file, times, execute) {
+  # Execute 'n' times
+  results <- vector(mode = "list", length = times)
+  
+  for (i in 1:times) {
+    results[[i]] <- execute(filename = file)
+  }
+  
+  # Initialize a problem instance for the analysis
+  problem <- initialize.problem(filename = file)
+  
+  # Analyze results
+  results_df <- local.analyze.results(results, problem)
+  
+  print(paste0("Best evaluation: ", round(min(results_df$Evaluation), 2), 
+               " - Mean: ", round(mean(results_df$Evaluation), 2), 
+               " - SD: ", round(sd(results_df$Evaluation), 2)), quote = FALSE)
+  print(paste0("Best runtime: ", round(min(results_df$Runtime), 2), 
+               " - Mean: ", round(mean(results_df$Runtime), 2), 
+               " - SD: ", round(sd(results_df$Runtime), 2)), quote = FALSE)
+  
+  # Print results in an HTML Table
+  kable_material(kbl(results_df, caption = paste(problem$name, sep = " - ")),
+                 c("striped", "hover", "condensed", "responsive"))
+}
+
+# Clear console
+cat("\014")
+graphics.off()
+
+file        <- "../data/one-pizza/a_an_example.in.txt"
+times       <- 5
+test(file, times, execute.hill.climbing)
+test(file, times, execute.local.beam.search)
+test(file, times, execute.random.restart.hill.climbing)
+test(file, times, execute.stochastic.hill.climbing)
+test(file, times, execute.stochastic.random.restart)
+
+file        <- "../data/one-pizza/b_basic.in.txt"
+times       <- 5
+test(file, times, execute.hill.climbing)
+test(file, times, execute.local.beam.search)
+test(file, times, execute.random.restart.hill.climbing)
+test(file, times, execute.stochastic.hill.climbing)
+test(file, times, execute.stochastic.random.restart)
